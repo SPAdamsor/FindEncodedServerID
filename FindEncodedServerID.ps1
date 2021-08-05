2b7279bdbd3e6855f8309adceb64241038157132
Adam Sorenson <46349808+SPAdamsor@users.noreply.github.com>
2021-08-05 17:31:53 -0500

    Update FindEncodedServerID.ps1

--- FindEncodedServerID.ps1
+++ FindEncodedServerID.ps1
@@ -1,45 +1,74 @@
-##The information that is  provided "as is" without warranty of any kind. We disclaim all warranties, either express or implied, including the warranties of merchantability and fitness for a particular purpose. In no event shall Microsoft Corporation or its suppliers be liable for any damages whatsoever including direct, indirect, incidental, consequential, loss of business profits or special damages, even if Microsoft Corporation or its suppliers have been advised of the possibility of such damages. Some states do not allow the exclusion or limitation of liability for consequential or incidental damages. Therefore, the foregoing limitation may not apply.
-##Find the server that issued the correlationID
-##Author:adamsor; https://adamsorenson.com
+##Find-EncodedServerID
+##Author:adamsor; adamsorenson.com
+##Version: 1.1
+##Time is encoded by GMT.  Timeoffset will change it to the server's time zone.  -7 would be PST
+##Example: Find-EncodedServerId 3c1f889f-096a-708b-2631-a9b8cbc3415e -TimeOffSet -7
 
 Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue
 $ErrorActionPreference = "SilentlyContinue"
 Function Find-EncodedServerId
 {
-param([guid]$corID)
+    param([guid]$corID,
+        [int]$TimeOffSet=0
+        )
+
 
         #This code calls to a Microsoft web endpoint to track how often it is used. 
         #No data is sent on this call other than the application identifier
         Add-Type -AssemblyName System.Net.Http
         $client = New-Object -TypeName System.Net.Http.Httpclient
         $cont = New-Object -TypeName System.Net.Http.StringContent("", [system.text.encoding]::UTF8, "application/json")
-        $tsk = $client.PostAsync("https://msapptracker.azurewebsites.net/api/Hits/6609208e-651e-4642-8247-4a0008a93958",$cont)
+        $tsk = $client.PostAsync("https://msapptracker.azurewebsites.net/api/Hits/22f91595-837b-4205-be08-8accea0eaf7d",$cont)
         #if you want to make sure the call completes, add this to the end of your code
         #$tsk.Wait()
 
-	$server = $null
+    $server = $null
 
-	$bytes = $corID.ToByteArray()
-	[int]$b8 = $bytes[8]
-	[int]$p1 = $b8 -shl 12
-	[int]$b9 = $bytes[9]
-	[int]$p2 = $b9 -shl 4
-	[int]$b10 = $bytes[10]
-	[int]$p3 = $b10 -shr 4
-	$esid= $p1 + $p2 + $p3
+    $bytes = $corID.ToByteArray()
+    [int]$b8 = $bytes[8]
+    [int]$p8 = $b8 -shl 12
+    [int]$b9 = $bytes[9]
+    [int]$p9 = $b9 -shl 4
+    [int]$b10 = $bytes[10]
+    [int]$p10 = $b10 -shr 4
+    $esid= $p8 + $p9 + $p10
 
-	$server = Get-SPServer  | ? {$_.EncodedServerID -eq $esid} 
-	$ad= $server.address
+    [int64]$b0 = $bytes[0]
+    [int64]$p0 = $b0 -shl 36
+    [int64]$b1 = $bytes[1]
+    [int64]$p1 = $b1 -shl 28
+    [int64]$b2 = $bytes[2]
+    [int64]$p2 = $b2 -shl 20
+    [int64]$b3 = $bytes[3]
+    [int64]$p3 = $b3 -shl 12
+    [int64]$b4 = $bytes[4]
+    [int64]$p4 = $b4 -shl 4
+    [int64]$b5 = $bytes[5]
+    [int64]$p5 = $b5 -shr 4
+    [int64]$44 = 52776558133248
+    [int64]$tsid= $p0 + $p1 + $p2 + $p3 + $p4 + $p5 + $44
+    
+    $timeGMT = Get-Date $($tsid*10000)
+    $offset = New-TimeSpan -Hours $TimeOffSet
+    $timeo = $timeGMT + $offset
+    $time = Get-Date $timeo  -Format "MMMM d, yyyy HH:mm:ss.fff"
+    
+    $server = Get-SPServer  | ? {$_.EncodedServerID -eq $esid} 
+    $ad= $server.address
 
 
-	If($server -ne $null)
-    	{
-    		Write-Host "CorrelationID $corID translates to $esid, $ad" -ForegroundColor Green
-    	}
-	Else
-    	{
-    		write-host "Did not find servers while decoding CorrelationID $corID please run the following PS:" -ForegroundColor Red
-    		write-host "Get-SPServer | select address, EncodedServerId" -ForegroundColor Green
-    		"EncodedServerID: " + $esid
-    	}
+    If($server -ne $null)
+    {
+        Write-Host "Estimated Server Time of the Correlation ID: $time" -ForegroundColor Yellow
+        Write-Host "CorrelationID $corID translates to $esid, $ad" -ForegroundColor Green
+    }
+    Else
+        {
+            Write-Host "Estimated Server Time of the Correlation ID: $time" -ForegroundColor Yellow
+            write-host "Did not find servers while decoding CorrelationID $corID please run the following PS:" -ForegroundColor Red
+            write-host "Get-SPServer | select address, EncodedServerId" -ForegroundColor Green
+            "EncodedServerID: " + $esid
+
+        }
 }
+####END
